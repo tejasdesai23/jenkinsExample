d986e5eb93cf349457da559007d2e3644e63c396
Tejas <tejasd1991@gmail.com>
2023-11-21 20:12:47 +0530

    Initial Commit

---
+++ src/test/java/com/spring/currencyexchangeservice/JenkinsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
